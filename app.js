590bcb226a2bdf2f1cce347c56e69bbbddf75799
Carter Plenge <124809586+CarterPlenge@users.noreply.github.com>
2024-11-06 23:43:50 -0600

    updated

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var boatRouter = require('./routes/boat');
+var gridRouter = require('./routes/grid');
+var pickRouter = require('./routes/pick');
 
 var app = express();
 
@@ -23,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/boat', boatRouter);
+app.use('/grid', gridRouter);
+app.use('/selector', pickRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
