d92d09d40d522d6d5fcc078215321adfeda547da
CarterPlenge <cplenge22@gmail.com>
2024-11-22 11:26:39 -0600

    lab 13

--- app.js
+++ app.js
@@ -5,6 +5,27 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var Boat = require("./models/boat");
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    Account.findOne({ username: username })
+      .then(function (user){
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+        return done(null, user);
+      })
+      .catch(function(err){
+        return done(err)
+      })
+  })
+)
 
 // We can seed the collection if needed on server start
 async function recreateDB(){
@@ -50,7 +71,6 @@ var boatRouter = require('./routes/boat');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
 var resourceRouter = require('./routes/resource');
-var boatRoutes = require('./routes/boat');
 
 var app = express();
 
@@ -62,18 +82,43 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/boats', boatRouter);
+app.use('/boat', boatRouter);
 app.use('/grid', gridRouter);
 app.use('/selector', pickRouter);
 app.use('/resource', resourceRouter);
-app.use('/boats', boatRoutes);
+app.use('/boats', boatRouter);
+
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser())
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
+  // passport config
+  // Use the existing connection
+  // The Account model
+  var Account =require('./models/account');
+  passport.use(new LocalStrategy(Account.authenticate()));
+  passport.serializeUser(Account.serializeUser());
+  passport.deserializeUser(Account.deserializeUser());
+
   next(createError(404));
 });
 
